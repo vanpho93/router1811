713fe2d6c9f9ef9236d183535fe241f6935a8aa9
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-01 19:56:15 +0700

    setup router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { DetailComponent } from './detail/detail.component';
+
+const routesConfig: Routes = [
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'detail', component: DetailComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routesConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
